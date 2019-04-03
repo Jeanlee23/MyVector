884c6e160d3d281a457a75b0dcf045d23c6d0dd8
Jeanlee <jeanlee-roy@hotmail.com>
2019-04-03 00:16:51 -0500

    Fixing technical problems

---
+++ cmake-build-debug/CMakeFiles/MyVector.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyVector.dir/main.cpp.obj"
+  "CMakeFiles/MyVector.dir/Vector.cpp.obj"
+  "MyVector.pdb"
+  "MyVector.exe"
+  "MyVector.exe.manifest"
+  "libMyVector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyVector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
